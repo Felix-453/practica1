da71d9b3bca07977d9d5adb837a6ca3dfd5ae3d7
Felix <al19020270@gmail.com>
2022-10-08 22:35:48 -0500

    second commit

--- src/App.js
+++ src/App.js
@@ -9,7 +9,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>Felix enrique chavez navarro</p>
         <a
+        
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
